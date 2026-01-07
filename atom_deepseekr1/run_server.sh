239400f8370c4f507d266b3df1344af10000c1a0
Chen Yi-Jun (Eveline) <167949857+yijuc@users.noreply.github.com>
2026-01-07 14:54:56 +0800

    csv modified

--- atom_deepseekr1/run_server.sh
+++ atom_deepseekr1/run_server.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-# ====== Setting TP, Profiler switch, output gemm switch =====
-TP=8  # or TP=4 (Currently deepseek-r1 cannot run with TP4)
+# ====== Setting TP, Profiler switch =====
+TP=8  # or TP=4
 enable_profiler=0
-enable_output_gemm=0
 # ========================
-
-server_log_dir="/workdir/atom_deepseekr1/logs_dsr1_atom_12241340"
-mkdir -p ${server_log_dir}
-log_tag="atom_fp8_tp${TP}_deepseek_r1"
-# log_tag="atom_fp8_tp${TP}_deepseek_r1_in1k_out1k_conc64_kernel"
-server_log_file="${server_log_dir}/${log_tag}_server_running.log"
 
 unset HIP_VISIBLE_DEVICES
 if [ "$TP" = "8" ]; then
@@ -21,39 +14,92 @@ else
     exit 1
 fi
 
-unset HIPBLASLT_LOG_FILE HIPBLASLT_LOG_MASK
-if [ "$enable_output_gemm" = "1" ]; then
-    export HIPBLASLT_LOG_FILE="${server_log_dir}/${log_tag}_gemm_output.log"
-    hipblaslt_log_dir=$(dirname "$HIPBLASLT_LOG_FILE")
-    mkdir -p "$hipblaslt_log_dir"
-    export HIPBLASLT_LOG_MASK=32
+client_log_dir="/workdir/atom_deepseekr1/logs_dsr1_atom_12241340"
+mkdir -p ${client_log_dir}
+log_tag="results"
+
+log_file=${1:-"benchmark_tp${TP}_${log_tag}.log"}
+csv_file=${2:-"benchmark_tp${TP}_${log_tag}.csv"}
+
+if [ "$enable_profiler" = "1" ]; then
+    log_file="profiler_${log_file}"
+    csv_file="profiler_${csv_file}"
 fi
 
-profiler_args=""
-if [ "$enable_profiler" = "1" ]; then
-    export VLLM_TORCH_PROFILER_WITH_STACK=1
-    export VLLM_TORCH_PROFILER_RECORD_SHAPES=1
-    profiler_dir="${server_log_dir}/${log_tag}"
-    mkdir -p $profiler_dir
-    profiler_args=" --torch-profiler-dir ${profiler_dir}"
+log_file="${client_log_dir}/${log_file}"
+csv_file="${client_log_dir}/${csv_file}"
+
+if [ ! -f "${csv_file}" ]; then
+    echo "TimeStamp,Input_Tokens,Output_Tokens,Max_Concurrency,Num_Prompts,Request_throughput_req_s,Mean_TTFT_ms,Mean_TPOT_ms,Token_Throughput" > "${csv_file}"
 fi
 
 # MODEL="/shared/amdgpu/home/share/deepseek/DeepSeek-R1-0528"
 MODEL="/dev/shm/DeepSeek-R1-0528"
 
-rm -rf /root/.cache/atom/
+echo "Input_Tokens,Output_Tokens,Max_Concurrency,Num_Prompts,Request_throughput_req_s,Mean_TTFT_ms,Mean_TPOT_ms,Token_Throughput" > ${csv_file}
 
-## 1k => add --max-model-len 2048
-# CMD="
-# python -m atom.entrypoints.openai_server --model $MODEL -tp $TP --block-size 16 --kv_cache_dtype fp8 --max-model-len 2048 $profiler_args 
-# "
-## 4k/10k
-CMD="
-python -m atom.entrypoints.openai_server --model $MODEL -tp $TP --block-size 16 --kv_cache_dtype fp8 $profiler_args 
-"
+PORT=8000
+configs=(
+    # "1024 1024 4"
+    # "1024 1024 8"
+    # "1024 1024 16"
+    # "1024 1024 32"
+    # "1024 1024 64"
+    # "1024 1024 128"
+    "4096 1024 64"
+    "4096 1024 128"
+    "10240 1024 32"
+    "10240 1024 64"
+    # "10240 1024 128"
+)
 
-{
-    echo "Running command: $CMD"
-    $CMD
+for config in "${configs[@]}"; do
+    read ISL OSL CONC <<< "$config"
+    num_prompts=$((CONC * 10))
+    RESULT_FILENAME="${client_log_dir}/deepseek_r1_FP8_tp${TP}_isl${ISL}_osl${OSL}_conc${CONC}_infrrate"
+    profiler_args=""
+    if [ "$enable_profiler" = "1" ]; then
+        RESULT_FILENAME="${RESULT_FILENAME}_profiler"
+        profiler_args=" --profile"
+    fi
 
-} 2>&1 | tee "$server_log_file"
+    timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+
+    echo "" | tee -a ${log_file}
+    echo "========================================" | tee -a ${log_file}
+    echo "Running benchmark at: ${timestamp}" | tee -a ${log_file}
+    echo "  Input tokens: ${ISL}" | tee -a ${log_file}
+    echo "  Output tokens: ${OSL}" | tee -a ${log_file}
+    echo "  Max concurrency: ${CONC}" | tee -a ${log_file}
+    echo "  Num prompts: ${num_prompts}" | tee -a ${log_file}
+    echo "  Resquest rate: inf" | tee -a ${log_file}
+    echo "========================================" | tee -a ${log_file}
+    
+    temp_output=$(mktemp)
+    # python /root/bench_serving/benchmark_serving.py \
+    PYTHONPATH=/ATOM/atom python -m benchmarks.benchmark_serving \
+    --model=$MODEL --backend=vllm --base-url=http://localhost:$PORT \
+    --dataset-name=random \
+    --random-input-len=${ISL} --random-output-len=${OSL} \
+    --random-range-ratio 0.8 \
+    --num-prompts=$num_prompts \
+    --max-concurrency=$CONC \
+    --request-rate=inf --ignore-eos \
+    --save-result --result-dir=${client_log_dir} --result-filename=$RESULT_FILENAME.json \
+    --percentile-metrics="ttft,tpot,itl,e2el" $profiler_args 2>&1 | tee -a ${log_file} | tee ${temp_output}
+
+    request_throughput=$(grep -i "Request throughput (req/s):" ${temp_output} | tail -1 | awk '{print $NF}')
+    mean_ttft=$(grep "Mean TTFT (ms):" ${temp_output} | tail -1 | awk '{print $4}')
+    mean_tpot=$(grep "Mean TPOT (ms):" ${temp_output} | tail -1 | awk '{print $4}')
+    token_throughput=$(grep "Total Token throughput (tok/s):" ${temp_output} | tail -1 | awk '{print $5}')
+    
+    echo "${timestamp},${ISL},${OSL},${CONC},${num_prompts},${request_throughput},${mean_ttft},${mean_tpot},${token_throughput}" >> ${csv_file}
+    
+    rm -f ${temp_output}
+    
+    echo "" | tee -a ${log_file}
+    echo "Completed at: $(date)" | tee -a ${log_file}
+    echo "========================================" | tee -a ${log_file}
+    
+    sleep 3
+done
